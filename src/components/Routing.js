52bfddc0f1853b8182d680f1950f933807bc7f03
topjohnwu <topjohnwu@gmail.com>
2016-06-27 12:37:29 +0800

    Add login routing

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import 'babel-polyfill';
 
+import Login from './Login';
+import Header from './Header';
+
 export default class Routing extends Component {
+  constructor(context) {
+    super(context);
+    this.state = {
+      user: this.get_user(__INITIAL_USER__),
+    }
+  }
+
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  };
+
+  componentWillMount() {
+    if(this.state.user === null)
+      this.context.router.push('/login');
+  }
+
+  get_user(session_id) {
+    console.log(session_id);
+    // TODO: Check if session id is available on server
+    // Temp solution: dummy user
+    if(session_id === null) return null;
+    else return {/*dummy for now*/};
+  }
+
+  Children() {
+    const children = React.Children.map(this.props.children,
+          (child) => React.cloneElement(child, {
+            user: this.state.user,
+          }));
+      return children;
+  }
+
   render() {
     return (
       <div>
-        <div>Routing</div>
+        {this.Children()}
       </div>
     )
   }
