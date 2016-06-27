fa57b09dbf4a3f3f4ae6d4a9959af9cbfb637b47
NTUwanderer <harveyycyang@gmail.com>
2016-06-27 15:22:43 +0800

    remove socket.io

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
-const socket = io.connect('http://localhost:8080/');
-console.log(socket);
-socket.on('msg', (data) => {
-  console.log(data); // eslint-disable-line no-console
-});
-socket.on('news', (data) => {
-  console.log(data); // eslint-disable-line no-console
-  socket.emit('my other event', { my: 'data' });
-});
 render(
   <Router history={browserHistory} routes={routes} />,
   document.getElementById('root')
